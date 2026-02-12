00dea7c5ce5b8d3f918765d68016355df7cfcd15
Pratham Nayak <protomn@users.noreply.github.com>
2026-02-12 18:03:55 +0530

    Defined a single stack frame in frame.hpp

---
+++ include/rbtrace/frame.hpp
@@ -0,0 +1,20 @@
+/*
+Core header and basic frame walker
+*/
+
+#pragma once
+#include <cstdint>
+
+namespace rbtrace
+{
+    // Defining a single stack frame
+    struct Frame
+    {
+        uintptr_t address; //Instruction pointer that returns the address
+
+        //Default constructor 
+        Frame() : address(0) { }
+
+        explicit Frame(uintptr_t a) : address(a) { }
+    };  
+}
